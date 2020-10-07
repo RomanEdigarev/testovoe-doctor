8afbd239d754cb340934eaf482bccb162ae3ade1
RomanEdigarev <romzes2_09@mail.ru>
2020-10-07 20:15:23 +0300

    flex for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React, {Provider} from 'react';
+import React from 'react';
 import './App.css';
-import NewCalendarContainer from "./components/new-calendar/NewCalendarContainer";
 import {BrowserRouter as Router} from 'react-router-dom'
 import {connect} from "react-redux";
 import Header from "./components/header/Header";
@@ -9,15 +8,15 @@ import Navbar from "./components/navbar/Navbar";
 
 function App() {
 
-  return (
-      <Router>
-          <div className="app">
-            {/*<NewCalendarContainer/>*/}
-            {/*  <Header/>*/}
-            <Navbar/>
-          </div>
-      </Router>
-  );
+    return (
+        <Router>
+            <div className="app">
+                {/*<NewCalendarContainer/>*/}
+                <Navbar/>
+                <Header/>
+            </div>
+        </Router>
+    );
 }
 
 const AppContainer = connect(null, null)(App)
