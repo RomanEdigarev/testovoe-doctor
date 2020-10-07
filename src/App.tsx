d2d8f673b436880adddeef3a6bed9446e1141bbe
RomanEdigarev <romzes2_09@mail.ru>
2020-10-07 15:31:39 +0300

    create header

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import './App.css';
 import NewCalendarContainer from "./components/new-calendar/NewCalendarContainer";
 import {BrowserRouter as Router} from 'react-router-dom'
 import {connect} from "react-redux";
+import Header from "./components/header/Header";
 
 
 function App() {
 
   return (
       <Router>
-          <div className="App">
+          <div className="app">
             {/*<NewCalendarContainer/>*/}
+              <Header/>
           </div>
       </Router>
   );
