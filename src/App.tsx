59f719d0ab82f5fea06e617de4090802f9e854c1
RomanEdigarev <romzes2_09@mail.ru>
2020-10-07 13:16:27 +0300

    create header

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
       <Router>
           <div className="App">
-            <NewCalendarContainer/>
+            {/*<NewCalendarContainer/>*/}
           </div>
       </Router>
   );
